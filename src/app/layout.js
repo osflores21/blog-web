e5101c2f2986ae79e5a66dbcb4aa32ffe3be7d3a
osflores21 <osflores67@gmail.com>
2024-01-17 19:46:41 -0600

    structure of the proyect

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import NavBar from '@/components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='h-screen '>
+          <NavBar />
+          <div className='mt-8 mx-auto p-4 justify-center'>
+            {children}
+
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
---
+++ src/components/BtmDelete.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BtmDelete = () => {
+  return (
+    <div>BtmDelete</div>
+  )
+}
+
+export default BtmDelete
