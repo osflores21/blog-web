a86cebef2c11ad2efb6e31870e9b59d60931b682
osflores21 <osflores67@gmail.com>
2024-01-19 03:07:50 -0600

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        smalt: {
+          50: '#f0f4fe',
+          100: '#dde6fc',
+          200: '#c3d3fa',
+          300: '#9ab8f6',
+          400: '#6a93f0',
+          500: '#476eea',
+          600: '#314fdf',
+          700: '#293dcc',
+          800: '#2733a6',
+          900: '#28348f',
+          950: '#1b1f50',
+        },
+      },
     },
   },
   plugins: [],
